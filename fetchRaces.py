13b25a8ee600cc26f89102bdd50d88a085d92916
pzia <paparazzia@gmail.com>
2018-05-06 22:30:22 +0200

    typo

--- fetchRaces.py
+++ fetchRaces.py
@@ -5,8 +5,8 @@ import fetchRace
 
 def run(fname = "listeRaces.txt"):
     with open(fname, 'r') as f:
-        for idr in f.readline():
-            fetchRace.archive(idr)
+        for idr in f.readlines():
+            fetchRace.archive(idr[:-1])
 
 if __name__ == "__main__":
     run()
