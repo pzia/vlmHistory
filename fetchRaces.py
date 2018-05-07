a875eb2d8ab9af84c5d7550760381018aab8f0e0
pzia <paparazzia@gmail.com>
2018-05-07 08:28:50 +0200

    multiprocessing

--- fetchRaces.py
+++ fetchRaces.py
@@ -2,11 +2,13 @@
 #-*- coding:utf-8 -*-
 
 import fetchRace
+import concurrent.futures
 
 def run(fname = "listeRaces.txt"):
     with open(fname, 'r') as f:
-        for idr in f.readlines():
-            fetchRace.archive(idr[:-1])
+        with concurrent.futures.ProcessPoolExecutor(max_workers=3) as executor:
+            for idr in f.readlines():
+                executor.submit(fetchRace.archive, idr[:-1])
 
 if __name__ == "__main__":
     run()
